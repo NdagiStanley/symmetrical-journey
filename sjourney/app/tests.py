24e51a1927d963162a079b504405c528f5db45c9
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-05 17:15:01 +0300

    [Fixes #118992849] PT-Github integration

--- sjourney/app/tests.py
+++ sjourney/app/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SetupTest(TestCase):
+    """docstring for SetupTest"""
+
+    def test(self):
+        """ab"""
+        self.assertEqual (1, 1)
--- sjourney/settings.py
+++ sjourney/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -70,6 +71,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sjourney.wsgi.application'
 
+# Use nose to run all tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# Tell nose to measure coverage on the app
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=app',
+]
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
