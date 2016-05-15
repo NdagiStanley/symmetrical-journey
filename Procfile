534fba4b7589ae0ea8b5577af4f41bdfa9f6b4f4
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-15 09:52:58 +0300

    [Feature #119493519] Models created and registered to admin

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py collectstatic --noinput;gunicorn sjourney.wsgi --pythonpath=sjourney --log-file -
+web: python manage.py collectstatic --noinput; python.manage.py migrate;gunicorn sjourney.wsgi --pythonpath=sjourney --log-file -
--- sjourney/app/models.py
+++ sjourney/app/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Picture(models.Model):
+    """docstring for Picture model"""
+
+    name = models.CharField(max_length=255)
+    size = models.CharField(max_length=255)
+    uploaded_image = models.ImageField()
+    editted_image = models.ImageField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    # Delete photos when uploader is deleted
+    uploader = models.ForeignKey(User, related_name="photos", on_delete=models.CASCADE)
