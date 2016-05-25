5886ff17a032c5eb6b0c71facf3a769745206331
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-25 23:27:50 +0300

    [Finishes #119494187] Facebook login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,5 @@
 <template>
   <div class="login">
-    {% if user and not user.is_anonymous %}
-    <a href="/logout/">
-      <div class="ui facebook button">
-        <i class="left arrow icon"></i>
-        <i class="facebook icon"></i>
-          Logout
-      </div>
-    </a>
-    <a href="/home/">
-      <div class="ui facebook button">
-        <i class="facebook icon"></i>
-          Go back to SJ
-        <i class="right arrow icon"></i>
-      </div>
-    </a>
-    {% else %}
     <a href="/login/facebook/">
       <div class="ui huge facebook button">
         <i class="facebook icon"></i>
@@ -23,7 +7,6 @@
         <i class="right arrow icon"></i>
       </div>
     </a>
-    {% endif %}
     <p>{{ msg }}</p>
   </div>
 </template>
