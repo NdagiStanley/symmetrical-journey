381179642faab5015c608e93737202824e68ea19
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-26 19:21:10 +0300

    [Feature #119493739] Dashboard layout with generic images

--- src/components/Steps.vue
+++ src/components/Steps.vue
@@ -3,49 +3,57 @@
         <div class="ui three top attached steps">
             <div class="active step">
               <div class="content">
-                <div class="title">Upload</div>
+                <div class="title"><i class="upload icon"></i>Upload</div>
                 <div class="description">Upload the picture</div>
               </div>
             </div>
             <div class="step">
               <div class="content">
-                <div class="title">Spice it up</div>
+                <div class="title"><i class="magic icon"></i>Spice it up</div>
                 <div class="description">Select effects to the photos</div>
               </div>
             </div>
             <div class="disabled step">
               <div class="content">
-                <div class="title">Share</div>
-                <div class="description">1-click share to facebook</div>
+                <div class="title"><i class="share icon"></i>Share</div>
+                <div class="description">1-click share to Facebook</div>
               </div>
             </div>
+        </div>
+        <div class="ui attached segment">
+          <p></p>
+        </div>
+        <div class="container">
+          <img class="main_image ui big image" src="//semantic-ui.com/images/wireframe/image.png">
+        </div>
+        <div class="ui segment">
+          <h2 class="ui right floated header">Effects</h2>
+          <div class="ui clearing divider"></div>
+          <div class="fx_images ui small images">
+            <img src="//semantic-ui.com/images/wireframe/image.png">
+            <img src="//semantic-ui.com/images/wireframe/image.png">
+            <img src="//semantic-ui.com/images/wireframe/image.png">
+            <img src="//semantic-ui.com/images/wireframe/image.png">
+            <img src="//semantic-ui.com/images/wireframe/image.png">
+            <img src="//semantic-ui.com/images/wireframe/image.png">
+            <img src="//semantic-ui.com/images/wireframe/image.png">
+            <img src="//semantic-ui.com/images/wireframe/image.png">
+            <img src="//semantic-ui.com/images/wireframe/image.png">
+            <img src="//semantic-ui.com/images/wireframe/image.png">
           </div>
-          <div class="ui attached segment">
-            <p></p>
-          </div>
-          <!-- <div class="ui three bottom attached steps">
-              <div class="step">
-                <i class="truck icon"></i>
-                <div class="content">
-                  <div class="title">Shipping</div>
-                  <div class="description">Choose your shipping options</div>
-                </div>
-              </div>
-              <div class="active step">
-                <i class="payment icon"></i>
-                <div class="content">
-                  <div class="title">Billing</div>
-                  <div class="description">Enter billing information</div>
-                </div>
-              </div>
-              <div class="disabled step">
-                <i class="info icon"></i>
-                <div class="content">
-                  <div class="title">Confirm Order</div>
-                  <div class="description">Verify order details</div>
-                </div>
-              </div>
-          </div> -->
         </div>
     </div>
 </template>
+
+<style type="text/css" scoped>
+.fx_images {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  height: 60%
+}
+
+</style>
