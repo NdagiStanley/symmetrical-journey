a17be8bcc3ca53f7b12bea8c68c2c32211c955f9
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-27 23:37:38 +0300

    [Feature #119493557] Add tests to the application

--- sjourney/app/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class SetupTest(TestCase):
-    """docstring for SetupTest"""
-
-    def test(self):
-        """ab"""
-        self.assertEqual (1, 1)
