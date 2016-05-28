a3e0fee4ffae4a6f9c05dc4517c23572e14163ca
Stanley Ndagi <stanley.ndagi@andela.com>
2016-05-28 23:35:00 +0300

    [Feature #120481309] Polish effects file

--- sjourney/app/effects.py
+++ sjourney/app/effects.py
@@ -1,38 +1,67 @@
 from PIL import Image, ImageFilter, ImageEnhance
 
-# Access the image
-image = Image.open('input.png')
 
-# FILTERS
+def Effect(effect, url):
+    """
+    Accepts effect as an integer
+    The url as a string
+    The return value should be an image
+    """
 
-image.filter(ImageFilter.DETAIL).show()
-image.filter(ImageFilter.BLUR).show()
-image.filter(ImageFilter.EMBOSS).show()
-image.rotate(180).show()
-image.filter(ImageFilter.FIND_EDGES).show()
-image.filter(ImageFilter.CONTOUR).show()
+    # Access the image and convert to RGBA to allow the filters
+    try:
+        image = Image.open(url).convert('RGBA')
+    except:
+        print("cannot open image file error")
+        return None
 
-# Increase contrast by 30%
-enh = ImageEnhance.Contrast(image)
-enh.enhance(1.3).show()
+    # Black and White
+    if effect == 1:
+        pic = image.convert("L")
 
-# Brighten
-image.point(lambda i: i * 1.2).show()
+    # FILTERS
+    elif effect == 2:
+        pic = image.filter(ImageFilter.DETAIL)
 
-# Black and White
-image.convert("L").show()
+    elif effect == 3:
+        pic = image.filter(ImageFilter.BLUR)
 
-# Pixelate
-backgroundColor = (0, ) * 3
-pixelSize = 9
-image = image.resize((image.size[0]/pixelSize, image.size[1]/pixelSize),
-                     Image.NEAREST)
-image = image.resize((image.size[0] * pixelSize, image.size[1] * pixelSize),
-                     Image.NEAREST)
-pixel = image.load()
-for i in range(0, image.size[0], pixelSize):
-    for j in range(0, image.size[1], pixelSize):
-        for r in range(pixelSize):
-            pixel[i + r, j] = backgroundColor
-            pixel[i, j + r] = backgroundColor
-image.save('output.png')
+    elif effect == 4:
+        pic = image.filter(ImageFilter.EMBOSS)
+
+    elif effect == 5:
+        pic = image.rotate(180)
+
+    elif effect == 6:
+        pic = image.filter(ImageFilter.FIND_EDGES)
+
+    elif effect == 7:
+        # pic = image.transpose(FLIP_LEFT_RIGHT)
+        pic = image.filter(ImageFilter.CONTOUR)
+
+    # Increase contrast by 30%
+    elif effect == 8:
+        enh = ImageEnhance.Contrast(image)
+        pic = enh.enhance(1.3)
+
+    # Brighten
+    elif effect == 9:
+        pic = image.point(lambda i: i * 1.2)
+
+    # Pixelate
+    elif effect == 10:
+        background_color = (0, ) * 3
+        pixel_size = 9
+        img = image.resize((image.size[0] / pixel_size,
+                            image.size[1] / pixel_size), Image.NEAREST)
+        img = img.resize((img.size[0] * pixel_size,
+                          img.size[1] * pixel_size), Image.NEAREST)
+        pixel = img.load()
+        for i in range(0, img.size[0], pixel_size):
+            for j in range(0, img.size[1], pixel_size):
+                for r in range(pixel_size):
+                    pixel[i + r, j] = background_color
+                    pixel[i, j + r] = background_color
+        pic = img
+
+    return pic
