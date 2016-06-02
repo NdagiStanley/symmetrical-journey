a802bb72d32e0d41872e4937f1ee14ec34d21f96
Stanley Ndagi <stanley.ndagi@andela.com>
2016-06-03 02:58:53 +0300

    [Feature #119494117] Create vuex store

--- src/store.js
+++ src/store.js
@@ -1,15 +1,24 @@
+import Vue from 'vue'
 import Vuex from 'vuex'
 
+// Make vue aware of Vuex
+Vue.use(Vuex)
+
+// Create an object to hold the initial state when
+// the app starts up
 const state = {
   picId: 0
 }
 
+// Create an object storing various mutations. We will write the mutation
 const mutations = {
   SELECTED (state) {
-    state.picId++
+    state.picId
   }
 }
 
+// Combine the initial state and the mutations to create a Vuex store.
+// This store can be linked to our app.
 export default new Vuex.Store({
   state,
   mutations
